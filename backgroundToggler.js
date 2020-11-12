47e7b33dfb1eddc77664deae143deaba02a81d4a
brisa-pedronetto <pedro@brisa.is>
2020-11-12 15:22:16 +0000

    Added css modules

--- backgroundToggler.js
+++ backgroundToggler.js
@@ -1,13 +1,15 @@
-const colors = ["antiquewhite", "tan", "tomato", "gold", "pink"];
+import colors from "./main.css";
 
 function getRandomColor() {
-  const randomIndex = Math.round(Math.random() * (colors.length - 1));
-  return colors[randomIndex];
+  const colorsValues = Object.values(colors);
+  const randomIndex = Math.round(Math.random() * (colorsValues.length - 1));
+  return colorsValues[randomIndex];
 }
 
 function toggleBgColor() {
-  const randomColor = getRandomColor();
-  document.body.style.backgroundColor = randomColor;
+  const randomColorClass = getRandomColor();
+  document.body.className = "";
+  document.body.classList.add(randomColorClass);
 }
 
 export { toggleBgColor };
--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 import { toggleBgColor } from "./backgroundToggler.js";
-// import classNames from "./main.css";
-
-// console.log(classNames.main);
 
 const toggler = document.querySelector("#toggler");
 toggler.addEventListener("click", () => {
